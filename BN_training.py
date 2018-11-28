f0f5b4326733afc9d9e16a4f20eda16498f8f803
Erling Ljunggren <erlingljunggren@gmail.com>
2018-11-28 16:20:24 +0000

    proof of concept learning

--- BN_training.py
+++ BN_training.py
@@ -1,3 +1,4 @@
+from  os import get_terminal_size
 import pgmpy as pgm
 from pgmpy.models import BayesianModel
 from pgmpy.factors.discrete import TabularCPD
@@ -6,50 +7,86 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from BN import create_BN
 
-G = create_BN()
+def row_estimate(G, index, estimated_data, latent_columns):
+    row = estimated_data.iloc[index]
+    row = row.drop(latent_columns)
 
-df  = pd.read_csv("data/BN/BN_discrete.csv",   sep=",", header=0)
-df_train, df_test = train_test_split(df, test_size=0.1) #500 rows for validating
+def combine(A, B):
+    "combines two dataframes with no common column"
+    for column in B.columns:
+        A[column] = B[column].values
+    return A
 
-#need this?
-df_train_target = df_train["Winner"]
-df_train_features = df_train.drop("Winner", axis=1)
-df_test_target = df_test["Winner"]
-df_test_features = df_test.drop("Winner", axis=1)
-print("Training")
-# Now that we are done with creating the model, we can train the CPD's
+def calc_accuracy(G, data):
+    winners = data["Winner"]
+    features = data.drop("Winner", axis=1)
+    n = len(data.index)
+    correct = 0
+    for i in range(1, n):
+        print("predicting winner: {:<5} of {}".format(i, n), end="\r")
+        prediction = G.predict(features.iloc[[i]])
+        predicted_winner = prediction["Winner"]
+        if predicted_winner.iloc[0] == winners.iloc[i]:
+            correct += 1
+    return correct / n
 
-#proof of consept limit
-n = 500
+# hidden_estimation = G.predict(limited_training_set)
+def predict_hidden(G, data):
+    n = len(data.index)
+    hidden_estimation = G.predict(data.iloc[[0]])
+    for i in range(1, n):
+        print("estimating row: {:<5} of {}".format(i, n), end="\r")
+        prediction = G.predict(data.iloc[[i]])
+        hidden_estimation = hidden_estimation.append(prediction, ignore_index=True)
+    return combine(hidden_estimation, data)
 
-#init
-prediction_change = True
-new_model = None
-old_model = G
+def main():
+    G = create_BN()
 
-limited_training_set = df_train.iloc[:n]
-limited_testing_set = df_train.iloc[:(n//10)]
+    df  = pd.read_csv("data/BN/BN_discrete.csv",   sep=",", header=0)
+    df_train, df_test = train_test_split(df, test_size=0.1) #500 rows for validating
 
-hidden_estimation = G.predict(limited_training_set.iloc[[0]])
-print(hidden_estimation.sample(1))
+    #proof of consept limit
+    n = 100
 
-#initialization of training set, need to have "complete" data
-print("initializing prediction 0")
-for i in range(1, n):
-    prediction = G.predict(limited_training_set.iloc[[i]])
-    hidden_estimation = hidden_estimation.append(prediction, ignore_index=True)
-print(hidden_estimation)
+    #init
+    prediction_change = True
 
-while prediction_change:
-    print("loop")
+    old_accuracy = 0
 
-    prediction_change = False
+    limited_training_set = df_train.iloc[:n]
+    limited_testing_set = df_test.iloc[:(n//10)]
 
 
+    # latent_columns = hidden_estimation.columns
 
-    # doesnt look like works with too much data
+    w, h = get_terminal_size()
+    i = 0
+    margin = 0.001
+    while prediction_change:
+        print("iteration: {}".format(i))
+        if i > 5000: break
+        # prediction_change = False
 
-    # GIBBS
-    # use model to sample from probability values for the four hidden variables | model.predict(uncomplete df)
-    # use new dataset to retrain the model  | model.fit(complete df)
+        # doesnt look like works with too much data
+        estimated_data = predict_hidden(G, limited_training_set)
+        G.fit(estimated_data)
+        accuracy = calc_accuracy(G, limited_testing_set)
+        print("accuracy: {}".format(accuracy))
+        if accuracy < old_accuracy + margin:break
+        old_accuracy = accuracy
 
+        # GIBBS
+        # use model to sample from probability values for the four hidden variables | model.predict(uncomplete df)
+        # use new dataset to retrain the model  | model.fit(complete df)
+
+        # #1 full iteration
+        # for z in range(n):
+        #     print("iteration: {:<4} - Gibbs: {:<4}".format(i, z), end="\r")
+        #     estimated_data.iloc[z] = row_estimate(G, z, estimated_data, latent_columns)
+
+        i += 1
+    print()
+
+if __name__ == "__main__":
+    main()
