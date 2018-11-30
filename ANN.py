a62cd31423ecd313b702d5674e660b47e7364fe9
majaarvola <majaarvola@gmail.com>
2018-11-30 17:25:10 +0900

    Update ANN.py
    added clock

--- ANN.py
+++ ANN.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import time
 
 df  = pd.read_csv("data/ANN/ANN_normalized.csv",   sep=",", header=0)
 df_train, df_test = train_test_split(df, test_size=0.1) #500 rows for validating
@@ -21,7 +22,11 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
       loss='sparse_categorical_crossentropy',
       metrics=['accuracy'])
 
+t1 = time.clock()
 model.fit(df_train_features.values, df_train_target.values, epochs=5)
+t2 = time.clock()
 
 test_loss, test_acc = model.evaluate(df_test_features.values, df_test_target.values)
 print('Test accuracy:', test_acc)
+print('Training time:', 
+"{:.2f}".format(t2-t1),'seconds')
